2315cbb1a262e9c5939920393b5fd4d360da281a
Charles Sprayberry <me@cspray.io>
2022-08-04 21:42:37 -0400

    Improve documented types

--- src/ObjectType.php
+++ src/ObjectType.php
@@ -8,4 +8,11 @@ namespace Cspray\Typiphy;
  * Implementations should take steps to ensure that non-object types cannot be represented by this interface. For example,
  * the type "string" is not an object and therefore should not be able to be represented as an ObjectType.
  */
-interface ObjectType extends Type {}
+interface ObjectType extends Type {
+
+    /**
+     * @return class-string
+     */
+    public function getName() : string;
+
+}
